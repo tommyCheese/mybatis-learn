a45f913fffa2fe88b4de1cb374ba7e4f9f08b317
Levi Shang <2516169500@qq.com>
2025-01-02 16:10:47 +0800

    mybatis基本配置

---
+++ mybatis-demo/src/main/java/com/levi/pojo/User.java
@@ -0,0 +1,60 @@
+package com.levi.pojo;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+    private  String gender;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
