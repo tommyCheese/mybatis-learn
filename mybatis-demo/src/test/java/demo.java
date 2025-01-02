c90ce47d0fd4b1abc3c8916bd314f1ddab45fa74
Levi Shang <2516169500@qq.com>
2025-01-02 16:59:55 +0800

    mybatis操作数据库的简单实现

--- mybatis-demo/src/test/java/demo.java
+++ mybatis-demo/src/test/java/demo.java
@@ -1,2 +1,26 @@
+import com.levi.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class demo {
+    public static void main(String[] args) throws IOException {
+        //1. 加载mybatis的核心配置文件，获取sqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//        System.out.println(sqlSessionFactory);
+
+        //2. 获取对应的sqlSession对象，用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3. 执行sql语句
+        List<User> users = sqlSession.selectList("test.selectUser"); // 命名空间+sql唯一标识
+        users.forEach(System.out::println);
+        sqlSession.close();
+    }
 }
