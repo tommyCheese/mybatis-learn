452a5698cba60d3c3529e39af72a8ec1b4777898
Levi Shang <2516169500@qq.com>
2025-01-02 22:08:56 +0800

    完成品牌信息的单个查询：三种条件设置方法：散装参数、对象参数和map参数

--- mybatis-demo/src/main/java/com/levi/mapper/BrandMapper.java
+++ mybatis-demo/src/main/java/com/levi/mapper/BrandMapper.java
@@ -1,8 +1,10 @@
 package com.levi.mapper;
 
 import com.levi.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     /**
@@ -15,5 +17,23 @@ public interface BrandMapper {
      * @param id
      * @return
      */
-    Brand selectBrandById(Integer id);
+    Brand selectById(Integer id);
+
+    /**
+     * 条件查询
+     * @param status
+     * @param companyName
+     * @param brandName
+     * @return
+     */
+    /**
+     * 条件查询参数接收方法
+     * 1. 散装参数：如果方法中有多个参数，需要使用@Param("SQL占位符名称")
+     * 2. 对象参数：对象的属性名称要和参数占位符一致
+     * 3. map参数：保证map的键值分别为sql参数名和值
+     *
+     */
+    List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+    List<Brand> selectByCondition(Brand brand);
+    List<Brand> selectByCondition(Map map);
 }
--- mybatis-demo/src/test/java/demo4.java
+++ mybatis-demo/src/test/java/demo4.java
@@ -7,7 +7,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 使用配置文件法完成单条数据获取
@@ -25,8 +27,31 @@ public class demo4 {
         //3. 执行sql语句
         //3.1 获取UserMapper接口的代理对象
         BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
-        Brand brand = mapper.selectBrandById(2);
-        System.out.println(brand);
+        int status = 1;
+        String companyName="华为";
+        String brandName = "华为";
+
+        // 处理模糊参数
+        companyName = "%"+companyName+"%";
+        brandName = "%"+brandName+"%";
+
+        // 条件查询
+        List<Brand> brands = mapper.selectByCondition(status,companyName,brandName);
+        System.out.println(brands);
+
+        Brand brandCondition = new Brand();
+        brandCondition.setStatus(status);
+        brandCondition.setCompanyName(companyName);
+        brandCondition.setBrandName(brandName);
+        List<Brand> brands1 = mapper.selectByCondition(brandCondition);
+        System.out.println(brands1);
+
+        Map mapCondition = new HashMap<>();
+        mapCondition.put("status",status);
+        mapCondition.put("companyName",companyName);
+        mapCondition.put("brandName",brandName);
+        List<Brand> brands2 = mapper.selectByCondition(mapCondition);
+        System.out.println(brands2);
         sqlSession.close();
     }
 }
